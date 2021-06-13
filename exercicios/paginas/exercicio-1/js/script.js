f7b418972181b023508a21b7f4ae34715392b626
Jardel Sousa <Jarbasrt81@gmail.com>
2021-06-13 19:23:00 -0300

    update

---
+++ exercicios/paginas/exercicio-1/js/script.js
@@ -0,0 +1,99 @@
+function carregar() {
+    var msgdia = window.document.getElementById('dia');
+    var msgmes = window.document.getElementById('mes');
+    var msgano = window.document.getElementById('ano');
+    var msgceu = window.document.getElementById('ceu');
+    var msghorario = window.document.getElementById('horario');
+    var msgsemana = window.document.getElementById('samana');
+    var img = window.document.getElementById('imagens');
+    var data = new Date();
+    var horas =  data.getHours();
+    var minutos = data.getMinutes();
+    var segundos = data.getSeconds();
+    var dia = data.getDate();
+    var semana = data.getDay();
+    var mes = data.getMonth();
+    var ano = data.getFullYear();
+
+    msgdia.innerHTML = dia;
+    msgano.innerHTML = ano;
+    msghorario.innerHTML = `${horas} : ${minutos} : ${segundos}`
+
+    switch (mes) {
+        case 0:
+            msgmes.innerHTML = 'Janeiro';
+        break
+        case 1:
+            msgmes.innerHTML = 'Fevereiro';
+        break
+        case 2:
+            msgmes.innerHTML = 'Março';
+        break
+        case 3:
+            msgmes.innerHTML = 'Abril';
+        break
+        case 4:
+            msgmes.innerHTML = 'Maio';
+        break
+        case 5: 
+            msgmes.innerHTML = 'Junho';
+        break
+        case 6:
+            msgmes.innerHTML = 'Julho';
+        break
+        case 7:
+            msgmes.innerHTML = 'Agosto';
+        break
+        case 8:
+            msgmes.innerHTML = 'Setembro';
+        break
+        case 9:
+            msgmes.innerHTML = 'Outubro';
+        break
+        case 10:
+            msgmes.innerHTML = 'Novembro';
+        break
+        case 11:
+            msgmes.innerHTML = 'Dezembro';
+    }
+
+    switch (semana) {
+        case 0:
+            msgsemana.innerHTML = 'Domingo'
+        break
+        case 1:
+            msgsemana.innerHTML ='Segunda'
+        break
+        case 2:
+            msgsemana.innerHTML = 'Terça'
+        break
+        case 3:
+            msgsemana.innerHTML = 'Quarta'
+        break
+        case 4: 
+            msgsemana.innerHTML = 'Quinta'
+        break
+        case 5: 
+            msgsemana.innerHTML = 'Sexta'
+        break
+        case 6:
+            msgsemana.innerHTML = 'Sábado'
+    } 
+
+    if (horas >= 0 && horas < 12) {
+        img.src = 'img/manha.jpg';
+        img.alt = 'imagem de bom dia.';
+        ceu.innerHTML = 'Manhã';
+        document.body.style.background = '#dcdadb';
+    } else if (horas >= 12 && horas <= 18) {
+        img.src = 'img/tarde.jpg';
+        img.alt = 'imagem de boa tarde.';
+        ceu.innerHTML = 'Tarde';
+        document.body.style.background = '#3f8aca';
+    } else {
+        img.src = 'img/noite.jpg';
+        img.alt = 'imagem de boa noite.';
+        ceu.innerHTML = 'Noite';
+        document.body.style.background = '#3d3d3d';
+    }
+}
---
+++ exercicios/paginas/exercicio-2/js/script.js
@@ -0,0 +1,53 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var txtAno = document.getElementById('txtano');
+    var res1 =  document.getElementById('res1');
+    var res2 =  document.getElementById('res2');
+    var sex = document.getElementsByName('radsex');
+    var idade = ano - Number(txtAno.value);
+    var genero = '';
+    var img = document.getElementById('foto');
+
+    
+
+    if (txtAno.value.length == 0 || txtAno.value > ano) {
+        alert('[ERRO] Idade invalida [ERRO]');
+    } else {
+
+        if (sex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 5) {
+                img.src = 'img/Mbb.jpg';
+            } else if (idade < 10) {
+                img.src = 'img/MCrianca.jpg';
+            } else if (idade < 18){
+                img.src = 'img/MJovem.jpg';
+            } else if (idade < 50){
+                img.src = 'img/Madulto.jpg';
+            } else if (idade < 120){
+                img.src = 'img/idoso.jpg';
+            } else {
+                img.src = 'img/Mzumbi.jpg';
+            }
+
+        } else if (sex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 5) {
+                img.src = 'img/Fbb.jpg';
+            } else if (idade < 10) {
+                img.src = 'img/FCrianca.jpg';
+            } else if (idade < 18){
+                img.src = 'img/FJovem.jpg';
+            } else if (idade < 50){
+                img.src = 'img/Fadulto.jpg';
+            } else if (idade < 120){
+                img.src = 'img/IDOSA.jpg';
+            } else {
+                img.src = 'img/Fzumbi.png';
+            }
+        }
+        res1.innerHTML = genero;
+        res2.innerHTML = `idade: ${idade}`;
+    }
+}
