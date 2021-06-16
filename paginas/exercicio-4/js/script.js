76947c155db35fac1a265eebd6a3abe928c6e3ef
Jardel Sousa <Jarbasrt81@gmail.com>
2021-06-16 19:49:13 -0300

    update

---
+++ paginas/exercicio-4/js/script.js
@@ -0,0 +1,19 @@
+function tab() {
+    let numero = document.getElementById('txtn');
+    let tabuada = document.getElementById('seltab');
+    if (numero.value.length == 0) {
+        alert('Por favor digite um numero valido!');
+    } else {
+        let n = Number(numero.value);
+        let c = 1;
+        tabuada.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
+            item.value = `tabuada${c}`;
+            tabuada.appendChild(item);
+            c++
+        }
+    }
+
+}
